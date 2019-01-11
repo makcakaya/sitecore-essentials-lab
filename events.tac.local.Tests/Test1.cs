5d8d4eeb030170a94e02d5ccfbe72b47a8959894
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2019-01-11 16:28:39 +0300

    Module 3 - Creating a Component

---
+++ events.tac.local.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace events.tac.local.Tests
+{
+    public sealed class Test1
+    {
+        [Fact]
+        public void FailedTests()
+        {
+            Assert.True(false);
+        }
+    }
+}
