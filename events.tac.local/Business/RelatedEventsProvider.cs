9614f72a5330314c4cee2f296daebea996d6d998
Mert Akcakaya <mert.akcakaya@osf-commerce.com>
2019-01-15 16:00:35 +0300

    Module 7 complete

---
+++ events.tac.local/Business/RelatedEventsProvider.cs
@@ -0,0 +1,29 @@
+﻿using events.tac.local.Models;
+using System.Collections.Generic;
+using System.Linq;
+using TAC.Sitecore.Abstractions.Interfaces;
+using TAC.Sitecore.Abstractions.SitecoreImplementation;
+
+namespace events.tac.local.Business
+{
+    public sealed class RelatedEventsProvider
+    {
+        private readonly IRenderingContext _context;
+
+        public RelatedEventsProvider(IRenderingContext context)
+        {
+            _context = context;
+        }
+
+        public RelatedEventsProvider() : this(SitecoreRenderingContext.Create())
+        {
+        }
+
+        public IEnumerable<NavigationItem> GetRelatedEvents()
+        {
+            return _context.ContextItem
+                 .GetMultilistFieldItems("relatedEvents")
+                 .Select(i => new NavigationItem(i.DisplayName, i.Url));
+        }
+    }
+}
---
+++ events.tac.local/Controllers/RelatedEventsController.cs
@@ -0,0 +1,24 @@
+﻿using events.tac.local.Business;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class RelatedEventsController : Controller
+    {
+        private readonly RelatedEventsProvider _provider;
+
+        public RelatedEventsController(RelatedEventsProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public RelatedEventsController() : this(new RelatedEventsProvider())
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View(_provider.GetRelatedEvents());
+        }
+    }
+}
